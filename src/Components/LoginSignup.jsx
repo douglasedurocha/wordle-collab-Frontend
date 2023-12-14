dd24ffde7408eb739f8d090d7c5d97c63a7d7c45
Douglas Eduardo Rocha <douglasedurocha@gmail.com>
2023-12-14 14:58:46 -0300

    add login and sign up authentication with wordle-collab API's accounts endpoint

--- src/Components/LoginSignup.jsx
+++ src/Components/LoginSignup.jsx
@@ -1,22 +1,78 @@
-import { Container, Grid, Paper, Tabs, Tab, TextField, Button, Input, IconButton, InputAdornment } from '@mui/material';
+import axios from 'axios';
+import cookies from 'react-cookies';
+
+import { 
+    Container,
+    Grid,
+    Typography,
+    Paper,
+    Tabs,
+    Tab,
+    TextField,
+    Button,
+    Input,
+    IconButton,
+    InputAdornment 
+} from '@mui/material';
 import VisibilityIcon from '@mui/icons-material/Visibility';
 import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
+
+
+axios.defaults.xsrfCookieName = 'csrftoken';
+axios.defaults.xsrfHeaderName = 'X-CSRFToken';
+axios.defaults.withCredentials = true;
+
+const client = axios.create({
+    // TODO: Put this in a .env file 
+    baseURL: "http://127.0.0.1:8000",
+    sameSite: 'none',
+});
 
 const LoginSignup = () => {
+    const [currentUser, setCurrentUser] = useState();
     const [action, setAction] = useState('login');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [showPass, setShowPass] = useState(false);
 
+    const fetchUserProfile = () => {
+        client.get("/api/accounts/user/", {
+            headers: {"X-CSRFToken": cookies.load('csrftoken')}
+        })
+        .then(function(res) {
+            setCurrentUser(true);
+        })
+        .catch(function(error) {
+            setCurrentUser(false);
+        });
+    };
+
+    useEffect(() => {
+        fetchUserProfile();
+    })
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+
+        client.post(`/api/accounts/${action}/`,{
+                email: email,
+                password: password
+            },
+            {
+                headers: {"X-CSRFToken": cookies.load('csrftoken')}
+            })
+            .then((res) => {
+                setCurrentUser(true);
+                console.log(res);
+            })
+            .catch(err => console.log(err));
+    }
+
     const handleChange = (event, newValue) => {
         setAction(newValue);
     };
-
-    const handleSubmit = (e) =>{
-        return null;
-    }
 
     return (
         <div>
@@ -30,8 +86,11 @@ const LoginSignup = () => {
                     style={{ minHeight: "100vh"}}
                 >
                     <Paper elevation={3} sx={{ p: 5}}>
+                        <Typography variant="h5" gutterBottom align="center">
+                            WELCOME
+                        </Typography>
                         <Grid item sx={{mb:4}}>
-                            <Tabs value={action} onChange={handleChange} aria-label="basic tabs example" centered>
+                            <Tabs value={action} onChange={handleChange} aria-label="Login or Sign up" centered>
                                 <Tab value="register" label="Register" />
                                 <Tab value="login" label="Login" />
                             </Tabs>
