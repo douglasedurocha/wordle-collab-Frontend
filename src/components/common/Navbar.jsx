8aa61f56d694f520bffc859d76bb83116c26394b
Douglas Eduardo Rocha <douglasedurocha@gmail.com>
2023-12-16 01:55:27 -0300

    update icons

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,8 +1,10 @@
-import { AppBar, Toolbar, Typography, Button } from '@mui/material';
+import { AppBar, Toolbar, Box, Typography, Button } from '@mui/material';
 
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { toast } from 'react-toastify';
+
+import Logo from "../../assets/logo.png";
 
 
 const Navbar = () => {
@@ -15,6 +17,15 @@ const Navbar = () => {
     return (
         <AppBar position="static">
             <Toolbar>
+                <Box
+                    component="img"
+                    sx={{
+                        height: 64,
+                        pr: 2,
+                    }}
+                    alt="Your logo."
+                    src={Logo}
+                />
                 <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                     Wordle Collab
                 </Typography>
